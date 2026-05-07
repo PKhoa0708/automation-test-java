1c1b2ef47f12fd9a2c2599d6e68c241c24f5857d
khoa.pham1 <khoap1862@gmail.com>
2026-05-07 16:16:59 +0700

    update 7/5

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions; // Import ChromeOptions
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -12,9 +13,16 @@ public class FirstTest {
 
     @BeforeMethod
     public void setUp() {
-        driver = new ChromeDriver();
+        // Cấu hình ChromeOptions cho chế độ headless
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Chạy Chrome ở chế độ không có giao diện người dùng
+        options.addArguments("--disable-gpu"); // Tắt GPU (thường cần thiết cho headless trên Linux)
+        options.addArguments("--no-sandbox"); // Tắt sandbox (thường cần thiết cho môi trường CI/CD)
+        options.addArguments("--window-size=1920,1080"); // Đặt kích thước cửa sổ ảo
+
+        driver = new ChromeDriver(options); // Truyền options vào ChromeDriver
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
+        driver.manage().window().maximize(); // Lệnh này có thể không có tác dụng trong headless, nhưng không gây hại
         driver.get("https://banve.my-board.org/auth/login.php");
         
         // Khởi tạo đối tượng LoginPage
